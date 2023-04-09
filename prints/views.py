e013e0c5e4128ebb19200794a7cda56163bb1018
Al <eganalan@gmail.com>
2023-04-09 19:43:01 +0000

    add search functions

--- prints/views.py
+++ prints/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import Print
 
 
@@ -6,7 +8,19 @@ def prints(request):
     """ A view to show all prints """
     prints = Print.objects.all()
 
-    return render(request, 'prints/prints.html', {'prints': prints, })
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(reqiest, "Please enter a proper search word")
+                return redirect(reverse('prints'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            prints = prints.filter(queries)
+
+    context = {'prints': prints, 'search_term': query, }
+    
+    return render(request, 'prints/prints.html', context)
 
 
 def single_print(request, pk):
