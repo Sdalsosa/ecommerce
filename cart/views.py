18fbddd2ea995f4aae53708e5d81514178455f13
Al <eganalan@gmail.com>
2023-04-15 19:01:45 +0000

    add add prints to cart function

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-
-""" Shopping Cart Page View """
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
+    """ Shopping Cart Page View """
+
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, pk):
+    """ add print quantity to cart """
+
+    quant = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('bag', {})
+
+    if pk in list(cart.keys()):
+        cart[pk] += quant
+    else:
+        cart[pk] = quant
+
+    request.session['cart'] = cart
+    print(cart)
+
+    return redirect(redirect_url)
