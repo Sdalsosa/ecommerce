71e99100a9582d5f597006550fdc719f2c7401e2
Al <eganalan@gmail.com>
2023-04-16 16:56:13 +0000

    add checkout functionality with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
+from django.conf import settings
 from .forms import OrderForm
+from cart.contexts import cart_content
+import stripe
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('prints'))
 
+    current_cart = cart_content(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+
+    )
+
+    print(intent)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key' : 'pk_test_51MxW5sEP3491zwROF1DHtkrbqPQIsrQlu9aHmpbIQeTvFdVC39pI6Iy0BxfQ0uBBiYAJ7Ayz2sQrpQXhHeSHpaH200uENvRC1Q',
-        'client_secret' : 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
