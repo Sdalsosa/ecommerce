79985ca9fb147e5d1f86d891853d219a128744cb
Al <eganalan@gmail.com>
2023-04-18 22:29:06 +0000

    fix settings.py

--- contactus/views.py
+++ contactus/views.py
@@ -8,8 +8,7 @@ def contactus(request):
         form = ContactUsForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Thank you for your message! \
-        We will get back to you shortly')
+            messages.success(request, f'Thank you for your message! We will get back to you shortly')
     else:
         form = ContactUsForm()
     return render(request, 'contactus/contactus.html', {'form': form})
