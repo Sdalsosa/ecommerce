95dd471297fd8a1280c40ed00cd7d292bf909057
Al <eganalan@gmail.com>
2023-04-19 23:57:07 +0000

    add custom 404 page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def error_404(request, exception):
+    return render(request, '404.html')
