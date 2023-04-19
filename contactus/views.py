33f6ce57ff4d8c5386faa7516c3c930823cc9df4
Al <eganalan@gmail.com>
2023-04-19 03:41:38 +0000

    edit contact us success redirect to home

--- contactus/views.py
+++ contactus/views.py
@@ -9,9 +9,10 @@ def contactus(request):
         if form.is_valid():
             form.save()
             messages.success(request, f'Thank you for your message! We will get back to you shortly')
+            return render(request, 'home/index.html')
     else:
         form = ContactUsForm()
-    return render(request, 'contactus/contactus.html', {'form': form})
+        return render(request, 'contactus/contactus.html', {'form': form})
 
 
 def about(request):
