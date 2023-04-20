05982e6eaee90a1b8ae50e68e0544f61df6f9a13
Al <eganalan@gmail.com>
2023-04-20 01:01:37 +0000

    error checking

--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
-from django.conf.urls import handler500
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,5 +33,3 @@ urlpatterns = [
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'ecommerce_site.views.handler404'
-handler500 = 'ecommerce_site.views.handler500'
