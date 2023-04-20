3dd071dc2378cc27c4a92f968baab899433ef987
Al <eganalan@gmail.com>
2023-04-20 00:09:53 +0000

    fix custom 404 page

--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -33,4 +33,4 @@ urlpatterns = [
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'home.views.error_404'
+handler404 = 'ecommerce_site.views.handler_404'
--- home/views.py
+++ home/views.py
@@ -5,7 +5,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
-
-
-def error_404(request, exception):
-    return render(request, '404.html')
